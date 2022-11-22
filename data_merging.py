4099b5cda62db905211a747bc3079b87cebdfa46
Wesley Janson <wrjanson@Wesleys-MacBook-Air.local>
2022-11-22 16:15:46 -0600

    Adding merging file

--- data_merging.py
+++ data_merging.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 
-# Read in "base" survey data
+# Read in "base" Michigan CFE survey data
 base_data = pd.read_csv("../paper_replication_data/MichiganConsumerSurvey.csv")
 
 # Read in Federal Funds Rate from FRED
@@ -20,13 +20,12 @@ cpi["CPIAUCSL"] = pd.Series(cpi["CPIAUCSL"]).pct_change(12)*100
 cpi_dur = pd.read_csv("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=CUSR0000SAD&scale")
 cpi_dur["CUSR0000SAD"] = pd.Series(cpi_dur["CUSR0000SAD"]).pct_change(12)*100
 
-
 # Read in Case-Shiller Index from FRED
 case = pd.read_csv("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=SPCS10RSA&scale=l")
 
-# Read in SPF 1-year ahead CPI from SPFspf = pd.read_excel("https://www.philadelphiafed.org/-/media/frbp/assets/surveys-and-data/survey-of-professional-forecasters/historical-data/inflation.xlsx?la=en&hash=F9C3E76769B4586C3E36E403DFA54BDC")
+# Read in SPF 1-year ahead CPI from SPF
 spf = pd.read_excel("https://www.philadelphiafed.org/-/media/frbp/assets/surveys-and-data/survey-of-professional-forecasters/historical-data/inflation.xlsx?la=en&hash=F9C3E76769B4586C3E36E403DFA54BDC")
-# Need to make monthly!!!
+# Need to change DATE to be YYYY-MM-DD
 
 ##### Merge All Data Together
 other_data = [fed_funds, unemp, cpi, cpi_dur, case, spf]
