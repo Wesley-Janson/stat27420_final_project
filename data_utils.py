e1b6f5710656d91921f0d565fc4ef05eb929dc5a
Drew Keller <drewkeller@uchicago.edu>
2022-12-03 13:24:10 -0600

    improved missing value handling

--- data_utils.py
+++ data_utils.py
@@ -21,28 +21,34 @@ def prep_features(
     data, regression=False, cts_vars=cts_vars, categorical_vars=categorical_vars,
     confounders=confounder_vars):
 
-    # outcome var -> cts if regression
+    # prep outcome
+    data['durable_purchase']=data['durable_purchase'].replace(to_replace={
+        'Good':1,'Neutral':0,'Bad':-1,"Don't know":np.nan,"Refused":np.nan})
+    # note: we could theoretically predict "don't know" for ML models, but there are so few we'll just exclude
+    # (could change in future)
+    print(f'Excluding {len(data[data.durable_purchase.isnull()])} observations' +
+        " that refused or didn't know durable purchase question.")
+    data = data[data.durable_purchase.notnull()]  # require outcome
+    categorical_vars = [var for var in categorical_vars if var != 'durable_purchase']
+
+    # handle regression vs. classification differences
     if regression: 
-        data['durable_purchase']=data['durable_purchase'].replace(to_replace={
-            'Good':1,'Neutral':0,'Bad':-1,"Don't know":np.nan,"Refused":np.nan})
-        print(f'Excluding {len(data[data.durable_purchase.isnull()])} observations' +
-            " that refused or didn't know durable purchase question.")
-        data = data[data.durable_purchase.notnull()]  # require outcome
-        categorical_vars = [var for var in categorical_vars if var != 'durable_purchase']
         temp = len(data)
         data = data.dropna(subset=confounders)
         print(f'Excluding {temp-len(data)} observations that did not answer confounder questions.')
     else:
-        data['durable_purchase']=data['durable_purchase'].replace(to_replace={
-            'Good':2,'Neutral':1,'Bad':0,"Don't know":3,"Refused":np.nan})
-        print(f'Excluding {len(data[data.durable_purchase.isnull()])} observations' +
-            ' that refused durable purchase question.')
-        data = data[data.durable_purchase.notnull()]  # require outcome
+        data["durable_purchase"] += 1  # code as {0,1,2} for XGBoost
+        for var in cts_vars:
+            if data[var].isnull().sum()>0 and var in confounders:
+                print(f'Imputing {data[var].isnull().sum()} missing values for {var} with median.')
+                data[var+'_imputed'] = data[var].isnull().astype(int)
+                data[var] = data[var].fillna(data[var].median())
     
-    # normalize cts variables
+    # prep cts variables
     data[cts_vars] = StandardScaler().fit_transform(data[cts_vars])
 
     # one-hot encode categorical variables, dropping first iff regression is True
+    data[categorical_vars] = data[categorical_vars].fillna('Missing')  # new class for missing
     data = pd.get_dummies(data, columns=categorical_vars, drop_first=regression)
 
     # prepare treatment and confounder var lists with dummies
@@ -56,4 +62,4 @@ def prep_features(
             if var in dummy:
                 confounder_vars.append(dummy)
 
-    return data, treatment_vars, confounder_vars
+    return data[other_vars+confounder_vars+treatment_vars+["durable_purchase"]], treatment_vars, confounder_vars
